2b1c3aaaaf97037a7a98477ad2feb60329dd7f67
brenonocciolizup <breno.noccioli@zup.com.br>
2021-08-24 17:19:24 -0300

    configuração keycloak

--- src/main/java/br/com/zupacademy/brenonoccioli/proposta/config/SecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/brenonoccioli/proposta/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package br.com.zupacademy.brenonoccioli.proposta.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
